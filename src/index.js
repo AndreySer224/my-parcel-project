cdfb2925fee9d846ea494fafc3688add1a950a36
Andrey Seredkin <164377947+AndreySer224@users.noreply.github.com>
2024-11-14 10:02:53 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -142,7 +142,6 @@ document.querySelectorAll('.apply-btn').forEach(button => {
 
    // Получаем все элементы с классом "icon heart"
    const icons = document.querySelectorAll('.icon.heard');
-
    // Назначаем обработчик клика для каждого элемента
    icons.forEach(icon => {
      icon.addEventListener('click', () => {
